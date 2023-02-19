c76714f138e8e4b1d885ff667258b375ede30c04
Dmitry <zweidmitr@gmail.com>
2023-02-19 21:00:41 +0300

    7

--- src/main/java/zweid/kyu_7/one/Alphabet.java
+++ src/main/java/zweid/kyu_7/one/Alphabet.java
@@ -9,6 +9,7 @@ public class Alphabet {
      I have four positive integers, A, B, C and D, where A < B < C < D.
      The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order.
      Your task is to return the value of D.
+     test git - new
     */
     public static int alphabet(int[] ns) {
         Arrays.sort(ns);
